7b0a12c6db0efa4862e8c421839a1dd184e6dc08
Matt Tingen <matt.tingen@willowtreeapps.com>
2018-03-28 16:46:33 -0400

    Add github script for linking branches

---
+++ src/github/index.js
@@ -0,0 +1,17 @@
+(function() {
+  const origin = 'https://github.com/';
+  function getLink(branch) {
+    const path = location.href.replace(origin, '');
+    const repo = path.match(/^([^\/]+\/[^\/]+)/)[0];
+    return `${origin}${repo}/tree/${encodeURIComponent(branch)}`;
+  }
+
+  const branches = $('.commit-ref');
+  branches.each(function() {
+    const el = $(this);
+    const branch = el.text();
+    const href = getLink(branch);
+    const link = $('<a>').attr('href', href);
+    el.wrapInner(link);
+  });
+})();
