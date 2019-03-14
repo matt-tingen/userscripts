05064d1dbff2155b5bc1dd39ad2ea67b786e6562
Matt Tingen <matt.tingen@gmail.com>
2019-03-13 20:57:58 -0400

    Fix Netflix intro skip

--- src/netflix/index.js
+++ src/netflix/index.js
@@ -1,28 +1,37 @@
-(function() {
-  const delayedArrive = (selector, callback) => {
+;(function() {
+  const arrive = (selector, callback) => {
     document.arrive(selector, function() {
-      setTimeout(callback.bind(this), 100);
-    });
+      setTimeout(callback.bind(this), 0)
+    })
   }
 
   // Avoid the "Are you still watching" interruption.
-  delayedArrive('.interrupter-actions', function() {
-    this.firstElementChild.click();
-  });
+  arrive('.interrupter-actions', function() {
+    this.firstElementChild.click()
+  })
 
   // Skip show intros.
-  delayedArrive('.skip-credits', function() {
-      this.firstElementChild.click();
-  });
+  arrive('.skip-credits', function() {
+    this.firstElementChild.click()
+
+    // The video is often paused after skipping credits so click the play button
+    // if it shows up shortly after doing so.
+    setTimeout(() => {
+      const playPauseButton = document.querySelector('.button-nfplayerPlay')
+
+      if (playPauseButton.getAttribute('aria-label') === 'Play') {
+        playPauseButton.click()
+      }
+    }, 100)
+  })
 
   // Play next episode when one completes.
-  delayedArrive('.WatchNext-still-container', function() {
-    this.click();
-  });
-
-  delayedArrive('.nf-flat-button-primary.nf-icon-button', function() {
+  arrive('.WatchNext-still-container', function() {
+    this.click()
+  })
+  arrive('.nf-flat-button-primary.nf-icon-button', function() {
     if (this.textContent.startsWith('Next episode')) {
-      this.click();
+      this.click()
     }
   })
-})();
+})()
