5f8a87af16eb5ac6eef8555dcd8c01423c30d7da
Matt Tingen <matt.tingen@gmail.com>
2019-05-03 19:59:56 -0400

    Automatically type package.json
    Since `resolveJsonModule` was removed in
    f469d7a320cf3c1b161e037b215b5a9e22d9022b, this adds back automatic
    typing of `package.json` through `json-ts`. The generated `package.d.ts`
    is ignored so that unrelated changes to `package.json` don't result in a
    typescript file being changed. If `package.d.ts` has not been generated,
    `packageInfo.ts` will have an error in it, but files which import
    `packageInfo` will have it typed as `any`, which is acceptable.

--- build/packageInfo.ts
+++ build/packageInfo.ts
@@ -1,9 +1,3 @@
-interface PackageInfo {
-  repository: string;
-  author: string;
-  homepage: string;
-}
-
 const packageInfo = require('../package.json') as PackageInfo;
 
 export default packageInfo;
