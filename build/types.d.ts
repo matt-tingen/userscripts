797149f6f1ea774d6fb255d1fceda4bfd2f79525
Matt Tingen <matt.tingen@gmail.com>
2019-06-10 22:45:14 -0400

    Prevent HN dark mode FoUC
    Perhaps "flash of understyled content"

--- build/types.d.ts
+++ build/types.d.ts
@@ -1,8 +1,16 @@
+type RunAtDestination =
+  | 'document-start'
+  | 'document-body'
+  | 'document-end'
+  | 'document-idle'
+  | 'context_menu';
+
 interface Metadata {
   [key: string]: string | string[];
   name: string;
   description: string;
   match: string[];
+  'run-at': RunAtDestination;
   require: string[];
   version: string;
 }
--- dist/hndark.user.js
+++ dist/hndark.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name HN Dark Mode
 // @namespace https://github.com/matt-tingen/userscripts
-// @version 1.0
+// @version 1.1
 // @author Matt Tingen
 // @description Dark theme
 // @downloadURL https://raw.githubusercontent.com/matt-tingen/userscripts/master/dist/hndark.user.js
 // @match https://news.ycombinator.com/*
 // @require https://raw.githubusercontent.com/matt-tingen/userscripts/master/src/utils/general.js
 // @require https://raw.githubusercontent.com/matt-tingen/userscripts/master/src/hackernews/dark.js
+// @run-at document-start
 // @grant none
 // ==/UserScript==
