c092577538d53ae1542eac54cd46d04771d42b1d
Matt Tingen <matt.tingen@gmail.com>
2019-06-13 21:30:37 -0400

    Fix hndark error
    If a tab is opened in the background, the userscript ran before `document.head` exists, causing the CSS injection to fail.

--- dist/hndark.user.js
+++ dist/hndark.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name HN Dark Mode
 // @namespace https://github.com/matt-tingen/userscripts
-// @version 1.2
+// @version 1.3
 // @author Matt Tingen
 // @description Dark theme
 // @downloadURL https://raw.githubusercontent.com/matt-tingen/userscripts/master/dist/hndark.user.js
 // @match https://news.ycombinator.com/*
 // @require https://raw.githubusercontent.com/matt-tingen/userscripts/master/src/utils/general.js
 // @require https://raw.githubusercontent.com/matt-tingen/userscripts/master/src/hackernews/dark.js
-// @run-at document-start
+// @run-at document-body
 // @grant none
 // ==/UserScript==
